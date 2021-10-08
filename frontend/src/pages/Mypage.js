e4d4fe6665cac6b18ed1e40cec97dad4b851ffb5
phseon <hye4659@gmail.com>
2021-10-08 18:44:13 +0900

    Delete Gender

--- frontend/src/pages/Mypage.js
+++ frontend/src/pages/Mypage.js
@@ -1,7 +1,7 @@
 import Title from '../components/Login/Title';
 import Profile from '../components/Mypage/Profile';
 import EmailPassword from '../components/Mypage/EmailPassword'
-import Gender from '../components/Mypage/Gender';
+//import Gender from '../components/Mypage/Gender';
 import Nickname from '../components/Mypage/Nickname';
 import React from 'react';
 
@@ -13,7 +13,7 @@ function Mypage() {
       <Profile/>
       <EmailPassword/>
       <Nickname/>
-      <Gender/>
+      {/*<Gender/>*/}
     </div>
   );
 }
