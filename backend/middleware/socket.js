a1370ac3b0fb0350913f6b9d229aa667e3810359
lightpurple <dmstjd10@gmail.com>
2021-12-08 22:09:56 +0900

    socket.io 수정

--- backend/middleware/socket.js
+++ backend/middleware/socket.js
@@ -1,12 +1,13 @@
 //const redis = require("socket.io-redis");
-const router = require("../routes/auth");
-
 var clients = [];
+const waiting = 0;
+const finding = 1;
 
 module.exports = (io) => {
     //io.adapter(redis({ host: "localhost", port: 6379 }));
     process.setMaxListeners(0);
     io.on("connection", (socket) => {
+        console.log("커넥션 성공!");
         socket.on("findChat", (data) => {
             if (!data.nick) {
                 socket.emit("Error", "닉네임이 없습니다.");
@@ -22,6 +23,7 @@ module.exports = (io) => {
                 nick: data.nick, // 사용자 닉네임
                 client: socket, // 사용자의 소켓
                 gender: data.gender,
+                status: finding,
             });
             socket.emit("userFinding"); // 로딩화면용
         });
@@ -37,32 +39,39 @@ module.exports = (io) => {
 
         socket.on("randomChatFinding", (data) => {
             for (var i = 0; i < clients.length; i++) {
-                if (clients[i].nick == data.nick) {
-                    continue;
-                } else if (clients[i].gender == data.match_gender) {
-                    var roomId = new Date().getTime() + "";
-                    clients[i].client.join(roomId);
-                    io.sockets.to(roomId).emit("userMatchingComplete", {
-                        roomId: roomId,
-                        other: clients[i].nick,
-                    });
-                    io.socket.to(roomId).emit("sysMsg", {
-                        message: `${clients[i].nick}님이 입장하셨습니다.`,
-                    });
-                    clients.splice(i, 1);
-                    return;
+                if (clients[i].status === finding) {
+                    if (clients[i].nick == data.nick) {
+                        continue;
+                    } else if (clients[i].gender == data.match_gender) {
+                        var roomId = new Date().getTime() + "";
+                        clients[i].status = waiting;
+                        clients[i].client.join(roomId);
+                        for (var j = 0; j < clients.length; j++) {
+                            if (clients[j].nick == data.nick) {
+                                clients[j].status = waiting;
+                                clients[j].client.join(roomId);
+                            }
+                        }
+                        io.to(roomId).emit("userMatchingComplete", {
+                            roomId: roomId,
+                        });
+                        io.to(roomId).emit("sysMsg", {
+                            message: "대화방에 입장하셨습니다!!",
+                        });
+                        return;
+                    }
                 }
             }
         });
 
         socket.on("message", (data) => {
-            io.sockets.to(data.roomId).emit("message", {
+            socket.to(data.roomId).emit("message", {
                 message: data.message,
             });
         });
 
         socket.on("disconnect", (data) => {
-            io.sockets.to(data.roomId).emit("sysMsg", {
+            socket.to(data.roomId).emit("sysMsg", {
                 message: `${data.nick}님이 퇴장하셨습니다.`,
             });
             socket.leave(data.roomId);
