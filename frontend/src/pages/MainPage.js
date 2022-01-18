e4daf20bb08bd51efa200f45c27d22b71b360664
phseon <hye4659@gmail.com>
2022-01-18 20:14:24 +0900

    create button - main

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -7,9 +7,9 @@ function Mainpage() {
     return (
     <div className="Mainpage">
         <Title title="가볍게 상대방과 하는 채팅"></Title>
-        <Link to="/chat">채팅</Link>
-        <Link to="/chatting">새 채팅</Link>
-        <Link to="/mypage">마이페이지</Link>
+        <Link to="/chat" className='cbutton'>채팅</Link>
+        <Link to="/chatting" className='ctbutton'>새 채팅</Link>
+        <Link to="/mypage"  className='mbutton'>마이페이지</Link>
         <ChatList/>
     </div>
     );
