2aaf63d27fbaea57a9a7d5e7ed3584bef7135954
phseon <hye4659@gmail.com>
2022-01-21 20:13:53 +0900

    Create User List

--- frontend/src/components/chat/ChatBox.js
+++ frontend/src/components/chat/ChatBox.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CSS/ChatBox.css";
+import List from './List'
 
-function ChatBox() {
+const ChatBox = () => {
+    const [modalOpen, setModalOpen] = useState(false)
+    const modalClose = () => {
+        setModalOpen(!modalOpen)
+        // Open()
+    }
+
+    // const [On, Off] = useState(false)
+    // const Open = () => {
+    //     Off(!On)
+    //     const co = document.getElementById("blo");
+    //     co.style.display = ("none" && "block");
+    // }
+
+
     return(
+    
+
     <div className="ChatBox">
         <div className="ChatRoom">
         <div className="YourChatbox">
@@ -14,10 +31,28 @@ function ChatBox() {
             <div className="MyImg"></div>
         </div>
         </div>
+
         <div className="FieldBox">
-            <div className="Field"></div>
+            <div className="Field"><input className="message"/>
+            </div>
             <div className="FieldButton"></div>
+            <div className="UserList">
+                <div className="User">
+                    <button className="YourProfile" onClick={modalClose}></button>
+                    <div id="user1" className="YourName"></div>
+                    {modalOpen && <List modalClose={modalClose}></List>}
+                    {/* div → p요소로 이름 나타내기 */}
+                    </div>
+
+                {/* 2번째 유저 - 1번째 유저와 모달 별개 수정 필요 */}
+                <div className="User">
+                    <button className="YourProfile" onClick={modalClose}></button>
+                    <div id="user2" className="YourName"></div>
+                    {modalOpen && <List modalClose={modalClose}></List>}
+                </div>
+            </div>
         </div>
+        
     </div>
     )
 }
---
+++ frontend/src/components/chat/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./CSS/ChatBox.css";
+
+const List = ({modalClose}) => {
+    return (
+        <div className="modal__container">
+            <button className="blk">차단</button>
+            <button className="exit">나가기</button>
+        </div>
+    )
+}
+
+export default List
