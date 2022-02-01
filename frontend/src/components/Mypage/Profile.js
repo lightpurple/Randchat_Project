94e605831718f59f77d28097e94e448d3cc941ab
phseon <hye4659@gmail.com>
2022-02-01 22:25:21 +0900

    modify profile

--- frontend/src/components/Mypage/Profile.js
+++ frontend/src/components/Mypage/Profile.js
@@ -7,31 +7,31 @@ import "./CSS/Profile.css";
 
 const Profile = () => {
 
-    const [fileUrl, setFileUrl] = useState(null);
+    // const [imageUrl, setFileUrl] = useState("");
 
-    const onChange = (e) => {
-        const ImageFile = e.target.files[0];
-        const image = new FormData();
-        const imageUrl = URL.createObjectURL(ImageFile);
-        setFileUrl(imageUrl)
-        image.append('image', ImageFile);
+    // const onClick = (e) => {
+    //     const ImageFile = e.target.files[0];
+    //     const image = new FormData();
+    //     const imageUrl = URL.createObjectURL(ImageFile);
+    //     setFileUrl(imageUrl)
+    //     image.append('image', ImageFile);
 
-        client.post('/api/mypage/upload', image)
-           .then(res => {
-                console.log(res);
-              })
-              .catch(err => {
-                console.error(err);
-              })
-    }
+    //     client.post('/api/mypage/upload', image)
+    //        .then(res => {
+    //             console.log(res);
+    //           })
+    //           .catch(err => {
+    //             console.error(err);
+    //           })
+    // }
 
-    const [url, setNickname] = useState("");
+    const [url, seturl] = useState("");
 
     useEffect(()=>{
-      client.get("/api/mypage/upload")
+      client.get("/api/mypage")
         .then(response => {
-        console.log(response.data.url);
-        setNickname(response.data.url);
+        console.log(response.data.image);
+        seturl(response.data.image);
       })
         .catch(error => {
         console.error(error);
@@ -55,8 +55,9 @@ const Profile = () => {
         
             <div className="imageBox">
                 <img className="image" src={url} alt=""/>
-                
             </div>
+
+
 
             <div className="Item">
                 <p>소개 한마디</p>
@@ -78,11 +79,21 @@ const Profile = () => {
             </div>
 
             <div className="itemSelect">
-                <label className="inputFileButton" htmlFor="file">
+                {/* <form method="post" action="/api/mypage/upload" enctype="multipart/form-data"> */}
+                {/* <label className="inputFileButton" htmlFor="file">
                     사진 선택
-                </label>
-                <input className="itemFile" type="file" id="file" onChange={onChange} />
+                </label> */}
+                {/* <input className="itemFile" type="file" id="file" accept="image/png, image/jpeg"/>
+                <input type="submit"/> */}
+
+                {/* </form> */}
                 {/* <button onClick={onClick}>저장하기</button> */}
+
+                <form action='/api/mypage/upload'>
+                <input className="itemFile" type="file" id="file" accept="image/png, image/jpeg"/>
+                <input type='submit' value='저장'/>
+                </form>
+
             </div>
         </div>
     );
