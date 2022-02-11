5d38c143bcf3f975e90524ee19a9c89008ac4b7f
phseon <hye4659@gmail.com>
2022-02-11 19:48:32 +0900

    design main footer

--- frontend/src/components/Auth/Footer.js
+++ frontend/src/components/Auth/Footer.js
@@ -1,14 +1,17 @@
 import React, {useState} from 'react';
 import './CSS/Footer.css';
+import { Link } from 'react-router-dom';
 
 export default function Tab() {
     const [currentTab, setTab]=useState(0);
 
-    const [currentCont, setCont]=useState(0);
+    const [currentCont1, setCont]=useState(false);
+    const [currentCont2, setContent]=useState(false);
+
 
     const tabClick=(index)=>{
         setTab(index);
-        setCont(index);
+        setCont(view => view ? false : true);setContent(view => view ? false : true);
     };
 
     const tabContArr=[
@@ -17,7 +20,17 @@ export default function Tab() {
                 <div className={currentTab===0 ? "active" : "unactive"} onClick={()=>tabClick(0)}>Developer</div>
             ),
             Content:(
-                <div className={currentCont===0 ? "current" : "none"}> 탭1 내용 </div>
+                <div className={currentCont1===false ? "none" : "content"}>
+                <div className='dev'>
+                <div className="web">FRONTEND</div>
+                <a href="https://github.com/jin0610" className='name'>서지인</a>
+                <a href="https://github.com/phseon" className='name'>박혜선</a>
+                </div>
+                <div className='dev'>
+                <div className="web">BACKEND</div>
+                <a href="https://github.com/lightpurple" className='name'>홍은성</a>
+                </div>
+                </div>
             )
         },
         {
@@ -25,7 +38,16 @@ export default function Tab() {
                 <div className={currentTab===1 ? "active" : "unactive"} onClick={()=>tabClick(1)}>About</div>
             ),
             Content:(
-                <div className={currentCont===1 ? "current" : "none"}>  탭2 내용 </div>
+                <div className={currentCont2===false ? "none" : "content"}><div className='abt'>
+                    <div className="about"> 새로운 사람을 만나기 어려워진 지금</div>
+                    <div className="about">
+                    온라인으로 친구를 만들어보세요</div>
+                    <div className="about">
+                    랜덤으로 매칭된 상대방과 대화를 시작하여</div>
+                    <div className="about">
+                    관심사를 공유하며 새로운 인연을 맺어볼 수 있습니다</div>
+                    </div>
+                    </div>
             )
         }
     ];
