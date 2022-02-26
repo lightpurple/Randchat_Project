d62f70415ec235d20407b3ac2e73585b2f3076db
jin0610 <sji302777@gmail.com>
2022-02-26 23:24:22 +0900

    임시

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ChatForm from '../containers/Chat/ChatForm';
 import ChatTitle from '../components/Chat/ChatTitle';
-import { withRouter, useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 
 const ChatPage = () => {
     const location = useLocation();
+    const history = useHistory();
     const info = location.state
     const socket = location.socket
+    if(location.socket === undefined){
+        history.push("/main");
+    }
+
     return(
         <div>
             <ChatTitle title="가볍게 상대방과 하는 채팅"></ChatTitle>
@@ -15,4 +20,4 @@ const ChatPage = () => {
     );
 }
 
-export default withRouter(ChatPage);
+export default React.memo(ChatPage);
--- frontend/src/pages/Mypage.js
+++ frontend/src/pages/Mypage.js
@@ -1,7 +1,6 @@
 import MyTitle from '../components/Mypage/MyTitle';
 import Profile from '../components/Mypage/Profile';
 import EmailPassword from '../components/Mypage/EmailPassword'
-//import Gender from '../components/Mypage/Gender';
 import Nickname from '../components/Mypage/Nickname';
 import UserDelete from '../components/Mypage/UserDelete';
 import Changepage from '../components/Mypage/Changepage';
